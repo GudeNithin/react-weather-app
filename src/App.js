96be52807ae6632198feef3fbd0b4df1255d48f9
Nithin Kumar Gude <95757938+GudeNithin@users.noreply.github.com>
2025-02-26 21:37:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
-import React,{useState} from 'react'
-import TodoList from './TodoList';
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  const [task,setTask]=useState("");
-  const [todos,setTodos]=useState([]);
+  const [city, setCity] = useState("");
+  const [result, setResult] = useState("");
 
-  const handler=e=>{
-    setTask(e.target.value);
-  }
+  const handler = (e) => {
+    setCity(e.target.value);
+  };
 
-  const onSubmitHandler=e=>{
+  const submitHandler = (e) => {
     e.preventDefault();
-    const newTodo=[...todos,task];
-    setTodos(newTodo);
-    setTask("");
-  }
-  
-  const deleteHandler=(indexVal)=>{
-    const newTodo=todos.filter((todo,index)=>index!==indexVal);
-    setTodos(newTodo);
-  }
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.main) {
+          const kelvin = data.main.temp;
+          const celcius = kelvin - 273.15;
+          setResult(`Temperature at ${city}: ${Math.round(celcius)}°C`);
+        } else {
+          setResult("City not found. Please try again.");
+        }
+        setCity("");
+      });
+  };
 
   return (
     <center>
-      <div>
-      <div className="card">
-        <div className="card-body">
-          <h2>Todo Management</h2>
-          <form onSubmit={onSubmitHandler}>
-            <input type="text" value={task} onChange={handler}/> &nbsp;
-            <input className="button" type="submit" name="Add" value="Add"/>
-          </form>
-          <TodoList todolist={todos} deleteHandler={deleteHandler}/>
-        </div>
+      <div className="container">
+      <div className="weather-card">
+        <h2 className="title">React Weather App</h2>
+        <form onSubmit={submitHandler} className="form">
+          <input
+            type="text"
+            placeholder="Enter City Name"
+            value={city}
+            onChange={handler}
+            className="input"
+          />
+          <button type="submit" className="button">
+            Get Temperature
+          </button>
+        </form>
+        <h3 className="result">{result}</h3>
       </div>
     </div>
     </center>
-  )
-}
+  );
+};
 
-export default App
+export default App;
